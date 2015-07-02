ef6685cc0392f7889f1d4a51b14af65e00aaf545
vladiibine <vlad.ardelean@3pillarglobal.com>
2015-07-02 21:20:24 +0300

    tests for proving checkpoint order

--- tests/test_pyvaldi.py
+++ tests/test_pyvaldi.py
@@ -3,7 +3,7 @@ import unittest
 from pyvaldi import ProcessStarter, Runner
 
 
-class TwoPhaseMachine(object):
+class ThreePhaseMachine(object):
     def __init__(self):
         self.steps = []
 
@@ -24,8 +24,8 @@ class TwoPhaseMachine(object):
 
 class TwoThreadsTestCase(unittest.TestCase):
     def test_first_thread_finishes_then_second_starts(self):
-        first_machine = TwoPhaseMachine()
-        second_machine = TwoPhaseMachine()
+        first_machine = ThreePhaseMachine()
+        second_machine = ThreePhaseMachine()
 
         first_starter = ProcessStarter(first_machine)
         cp1 = first_starter.add_checkpoint_after(first_machine.second_phase)
@@ -41,10 +41,68 @@ class TwoThreadsTestCase(unittest.TestCase):
         self.assertIs(next(runner), cp2)
         self.assertEqual(second_machine.steps, [1, 2, 3])
 
+    def test_checkpoint_order_simple_2_procs_3_checkpoints_each(self):
+        # Time -> ...
+        # Process 1: CP1   CP2   CP3
+        # Process 2:                      CP1   CP2   CP3
+        machine1 = ThreePhaseMachine()
+        machine2 = ThreePhaseMachine()
+
+        starter1 = ProcessStarter(machine1)
+        starter2 = ProcessStarter(machine2)
+
+        cp1_1 = starter1.add_checkpoint_before(machine1.first_phase)
+        cp1_2 = starter1.add_checkpoint_before(machine1.second_phase)
+        cp1_3 = starter1.add_checkpoint_before(machine1.third_phase)
+
+        cp2_1 = starter2.add_checkpoint_before(machine2.first_phase)
+        cp2_2 = starter2.add_checkpoint_before(machine2.second_phase)
+        cp2_3 = starter2.add_checkpoint_before(machine2.third_phase)
+
+        runner = Runner(
+            [starter1, starter2],
+            [cp1_1, cp1_2, cp1_3, cp2_1, cp2_2, cp2_3])
+
+        self.assertIs(next(runner), cp1_1)
+        self.assertIs(next(runner), cp1_2)
+        self.assertIs(next(runner), cp1_3)
+        self.assertIs(next(runner), cp2_1)
+        self.assertIs(next(runner), cp2_2)
+        self.assertIs(next(runner), cp2_3)
+
+    def test_checkpoint_order_mixed_2_procs_3_checkpoints_each(self):
+        # Time -> ... (checkpoints order)
+        # Process 1:  CP1      CP2   CP3
+        # Process 2:      CP1             CP2 CP3
+        machine1 = ThreePhaseMachine()
+        machine2 = ThreePhaseMachine()
+
+        starter1 = ProcessStarter(machine1)
+        starter2 = ProcessStarter(machine2)
+
+        cp1_1 = starter1.add_checkpoint_before(machine1.first_phase)
+        cp1_2 = starter1.add_checkpoint_before(machine1.second_phase)
+        cp1_3 = starter1.add_checkpoint_before(machine1.third_phase)
+
+        cp2_1 = starter2.add_checkpoint_before(machine2.first_phase)
+        cp2_2 = starter2.add_checkpoint_before(machine2.second_phase)
+        cp2_3 = starter2.add_checkpoint_before(machine2.third_phase)
+
+        runner = Runner(
+            [starter1, starter2],
+            [cp1_1, cp2_1, cp1_2, cp1_3, cp2_2, cp2_3])
+
+        self.assertIs(next(runner), cp1_1)
+        self.assertIs(next(runner), cp2_1)
+        self.assertIs(next(runner), cp1_2)
+        self.assertIs(next(runner), cp1_3)
+        self.assertIs(next(runner), cp2_2)
+        self.assertIs(next(runner), cp2_3)
+
 
 class SingleProcessTestCase(unittest.TestCase):
     def test_single_checkpoint_is_returned_by_runner(self):
-        machine = TwoPhaseMachine()
+        machine = ThreePhaseMachine()
 
         starter = ProcessStarter(machine)
         cp1 = starter.add_checkpoint_before(machine.first_phase)
@@ -53,7 +111,7 @@ class SingleProcessTestCase(unittest.TestCase):
         self.assertIs(next(runner), cp1)
 
     def test_3_checkpoints_are_returned_by_runner(self):
-        machine = TwoPhaseMachine()
+        machine = ThreePhaseMachine()
 
         starter = ProcessStarter(machine)
         cp1 = starter.add_checkpoint_before(machine.first_phase)
@@ -65,4 +123,3 @@ class SingleProcessTestCase(unittest.TestCase):
         self.assertIs(next(runner), cp1)
         self.assertIs(next(runner), cp2)
         self.assertIs(next(runner), cp3)
-
