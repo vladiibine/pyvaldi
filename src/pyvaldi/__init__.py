ea707a194de2a02cdc845fe981742b1f69c79b6a
vladiibine <vlad.ardelean@3pillarglobal.com>
2015-07-14 23:24:30 +0300

    refactored and added todos

--- src/pyvaldi/__init__.py
+++ src/pyvaldi/__init__.py
@@ -13,6 +13,7 @@ class ProcessStarter(object):
         self.args = args
         self.kwargs = kwargs
         self.event = None
+        self._terminal_checkpoint = Checkpoint(self, None)
 
     def add_checkpoint_after(self, callable_):
         """Create and return a checkpoint, set AFTER the callable returns"""
@@ -21,6 +22,10 @@ class ProcessStarter(object):
     def add_checkpoint_before(self, callable_):
         """Create and return a checkpoint, set BEFORE the callable returns"""
         return Checkpoint(self, callable_, before=True)
+
+    def get_terminal_checkpoint(self):
+        """Returns a checkpoint that marks the process end"""
+        return self._terminal_checkpoint
 
 
 class Runner(object):
@@ -39,7 +44,9 @@ class Runner(object):
             starter: ThreadConductor(
                 target=starter.callable,
                 args=starter.args,
-                kwargs=starter.kwargs)
+                kwargs=starter.kwargs,
+                terminal_checkpoint=starter.get_terminal_checkpoint()
+            )
             for starter in starters
         }
 
@@ -48,14 +55,21 @@ class Runner(object):
         next_checkpoint, last_checkpoint = self._get_next_and_last_checkpoint(
             self.checkpoints, self._next_checkpoint_idx
         )
+        # TODO - coordination here is required to step over terminal
+        # checkpoints. Just need to call _run_single_step for the intermediary
+        # checkpoints, and we're done
 
-        runnable_conductor = self._get_runnable_conductor(
-            last_checkpoint, next_checkpoint)
-
-        runnable_conductor.start_or_continue(next_checkpoint)
+        self._run_single_step(last_checkpoint, next_checkpoint)
 
         self._next_checkpoint_idx += 1
         return next_checkpoint
+
+    def _run_single_step(self, last_checkpoint, next_checkpoint):
+        if next_checkpoint is last_checkpoint is None:
+            raise StopIteration
+        runnable_conductor = self._get_runnable_conductor(
+            last_checkpoint, next_checkpoint)
+        runnable_conductor.start_or_continue(next_checkpoint)
 
     def _get_runnable_conductor(self, last_checkpoint, next_checkpoint):
         """Return the ThreadConductor that should run next"""
@@ -110,10 +124,13 @@ class Checkpoint(object):
         self.before = before
 
     def get_code(self):
+        if not self.callable:
+            return
         return self.callable.func_code
 
 
 class SleepyProfiler(object):
+    """Profiler that will wait when it reaches a certain checkpoint """
     def __init__(self, checkpoint, condition, checkpoint_reached_callback):
         self.confirming_checkpoints = [checkpoint]
         self.checkpoint_idx = 0
@@ -142,34 +159,39 @@ class SleepyProfiler(object):
             self.checkpoint_idx += 1
 
     def add_checkpoint(self, checkpoint):
+        """Set a new checkpoint where the profiler should wait """
         self.confirming_checkpoints.append(checkpoint)
 
 
-class ProfilingThread(threading.Thread):
-    def __init__(self, target, args, kwargs, condition):
-        super(ProfilingThread, self).__init__(target=target, args=args, kwargs=kwargs)  # noqa
+class SleepingThread(threading.Thread):
+    """Thread that will stop and sleep at the checkpoints specified """
+    def __init__(self, target, args, kwargs, condition, terminal_checkpoint):
+        super(SleepingThread, self).__init__(target=target, args=args, kwargs=kwargs)  # noqa
         self.condition = condition
         self.checkpoint = None
         self.profiler = None
+        self.terminal_checkpoint = terminal_checkpoint
         self._checkpoints_reached = []
         self._checkpoint_edit_lock = threading.Lock()
 
     def set_checkpoint(self, checkpoint):
+        """Specify a checkpoint that when reached, will cause the thread to sleep
+        """
         self.checkpoint = checkpoint
         if self.profiler:
             self.profiler.add_checkpoint(checkpoint)
 
     def run(self):
         self.profiler = SleepyProfiler(
-            self.checkpoint, self.condition, self.checkpoint_reached_callback)
+            self.checkpoint, self.condition, self.confirm_checkpoint_reached)
 
         sys.setprofile(self.profiler)
-        super(ProfilingThread, self).run()
+        super(SleepingThread, self).run()
 
-        # hardcoded...yes... but the None checkpoint means the thread is done
-        self.checkpoint_reached_callback(None)
+        # self.confirm_checkpoint_reached(self.terminal_checkpoint)
+        self.confirm_checkpoint_reached(None)
 
-    def checkpoint_reached_callback(self, checkpoint):
+    def confirm_checkpoint_reached(self, checkpoint):
         self._checkpoint_edit_lock.acquire()
         self._checkpoints_reached.append(checkpoint)
         self._checkpoint_edit_lock.release()
@@ -183,19 +205,28 @@ class ProfilingThread(threading.Thread):
 
 class ThreadConductor(object):
     """Allows a thread to run intermittently (until it reaches checkpoints) """
-    def __init__(self, target, args, kwargs):
+    def __init__(self, target, args, kwargs, terminal_checkpoint):
         # We wouldn't want the default re-entrant lock now, would we?
         self.condition = threading.Condition(lock=threading.Lock())
-        self.thread = ProfilingThread(
-            target=target, args=args, kwargs=kwargs, condition=self.condition)
+        self.thread = SleepingThread(
+            target=target,
+            args=args,
+            kwargs=kwargs,
+            condition=self.condition,
+            terminal_checkpoint=terminal_checkpoint
+        )
         self.thread.setDaemon(True)
         self._started = False
 
     def start_or_continue(self, next_checkpoint):
         """Lets associated thread run until it reaches the :param:`next_checkpoint`
 
-        :param next_checkpoint:
+        :param Checkpoint next_checkpoint: the next checkpoint that must be
+            reached
         """
+        # TODO - next_checkpoint will never be none here. We'll only
+        # get regular and terminal checkpoints
+        # (maybe initial checkpoints? we don't have those yet)
         self.thread.set_checkpoint(next_checkpoint)
 
         if not self._started:
@@ -206,10 +237,6 @@ class ThreadConductor(object):
             while not self.thread.has_reached_checkpoint(next_checkpoint) and self.thread.is_alive():
                 time.sleep(0.1)
         else:
-            # # wait for the thread to hit the last checkpoint (Join)
-            # while not self.thread.has_reached_checkpoint(last_checkpoint) and self.thread.is_alive():
-            #     time.sleep(0.1)
-
             self.condition.acquire()
             # Notify other thread to continue its execution
             self.condition.notify()
